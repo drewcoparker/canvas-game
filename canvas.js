79f81829b161449dcd79c7ec280544879843f3a2
Drew Parker <drew.i.parker@gmail.com>
2016-12-07 08:36:42 -0500

    Monster now gives chase

--- canvas.js
+++ canvas.js
@@ -29,9 +29,13 @@ var monster = new Image();
 monster.src = "assets/monster.png"
 var monsterLocation = {
     x: 200,
-    y: 100
+    y: 100,
+    newX: 200,
+    newY: 100
 }
 
+// This is used to speed up or slow down the hero when arrow keys are pressed.
+var speedModifier = .5;
 
 // an array to hold all the keys that are currently pressed down
 var keysDown = {};
@@ -48,21 +52,31 @@ addEventListener('keyup', function(event) {
 
 function update() {
     // this is where we will update the hero based on what is true in keysDown
+
+    // boundary coordinates: 10, 455, 413, 22
     // if keyDown[38] is true, then user pressed the up arrow
     if (38 in keysDown) {
-        heroLocation.y -= 10;
+        if (heroLocation.y >= 10) {
+            heroLocation.y -= (10 * speedModifier);
+        }
     }
     // if keyDown[39] is true, then user pressed the right arrow
     if (39 in keysDown) {
-        heroLocation.x += 10;
+        if (heroLocation.x <= 455) {
+            heroLocation.x += (10 * speedModifier);
+        }
     }
     // if keyDown[40] is true, then user pressed the down arrow
     if (40 in keysDown) {
-        heroLocation.y += 10;
+        if (heroLocation.y <= 413) {
+            heroLocation.y += (10 * speedModifier);
+        }
     }
     // if keyDown[37] is true, then user pressed the left arrow
     if (37 in keysDown) {
-        heroLocation.x -= 10;
+        if (heroLocation.x >= 22) {
+            heroLocation.x -= (10 * speedModifier);
+        }
     }
     // if keyDown[33] is true, then user pressed the space bar
     if (33 in keysDown) {
@@ -74,15 +88,43 @@ function update() {
         (monsterLocation.x <= heroLocation.x + 32) &&
         (monsterLocation.y <= heroLocation.y + 32)
     ){
-        console.log("The hero is on top of the goblin.");    
+        console.log("The hero is on top of the goblin.");
     } else {
         console.log("The hero is not close enough.");
+    }
+}
+
+function monsterMoves() {
+    if ((monsterLocation.newX === monsterLocation.x) &&
+        (monsterLocation.newY === monsterLocation.y)
+    ){
+        // Uncomment to have the monster roam randomly
+        // monsterLocation.newX = Math.floor(Math.random() * 440);
+        // monsterLocation.newY = Math.floor(Math.random() * 440);
+
+        // Uncomment to have the monster follow hero
+        monsterLocation.newX = heroLocation.x;
+        monsterLocation.newY = heroLocation.y;
+    } else {
+        if (monsterLocation.newX > monsterLocation.x) {
+            monsterLocation.x += (1 * 1);
+        }
+        if (monsterLocation.newX < monsterLocation.x) {
+            monsterLocation.x -= (1 * 1);
+        }
+        if (monsterLocation.newY > monsterLocation.y) {
+            monsterLocation.y += (1 * 1);
+        }
+        if (monsterLocation.newY < monsterLocation.y) {
+            monsterLocation.y -= (1 * 1);
+        }
     }
 }
 
 // Keep this function at the bottom
 function draw() {
     update();
+    monsterMoves();
     // Put our background image on the context at x=0, y=0
     context.drawImage(backgroundImage, 0, 0);
     context.drawImage(hero, heroLocation.x, heroLocation.y);
