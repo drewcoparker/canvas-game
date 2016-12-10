03cd1905c06cef4fb372edb36f713f8c41486e68
Drew Parker <drew.i.parker@gmail.com>
2016-12-10 14:24:49 -0500

    Added collision function

--- canvas2.js
+++ canvas2.js
@@ -1,35 +1,34 @@
 
 // Character constructors
-function Hero(image, speed) {
+function Hero(image) {
     this.image = new Image();
     this.image.src = image;
     this.x = Math.floor(Math.random() * 440);
     this.y = Math.floor(Math.random() * 440);
-    this.speed = speed;
 }
 
-Hero.prototype.update = function(keyDownEvent) {
+Hero.prototype.update = function(keyDownEvent, speedModifier=5) {
     if (38 in keyDownEvent) {
         if (this.y >= 10) {
-            this.y -= (10 * this.speed);
+            this.y -= speedModifier;
         }
     }
     // if keyDown[39] is true, then user pressed the right arrow
     if (39 in keyDownEvent) {
         if (this.x <= 455) {
-            this.x += (10 * this.speed);
+            this.x += speedModifier;
         }
     }
     // if keyDown[40] is true, then user pressed the down arrow
     if (40 in keyDownEvent) {
         if (this.y <= 413) {
-            this.y += (10 * this.speed);
+            this.y += speedModifier;
         }
     }
     // if keyDown[37] is true, then user pressed the left arrow
     if (37 in keyDownEvent) {
         if (this.x >= 22) {
-            this.x -= (10 * this.speed);
+            this.x -= speedModifier;
         }
     }
 }
@@ -66,6 +65,36 @@ Monster.prototype.movement = function(speed) {
 }
 
 // *****************************************************************************
+// Game functions
+function collision() {
+    for (var i = 0; i < monsters.length; i++) {
+        if ((hero.x <= monsters[i].x + 32) &&
+            (hero.y <= monsters[i].y + 32) &&
+            (monsters[i].x <= hero.x + 32) &&
+            (monsters[i].y <= hero.y + 32)
+        ){
+            console.log("collision");
+        }
+    }
+        // console.log("The hero is on top of the goblin.");
+        // currentPlayersScore++;
+        //
+        // document.getElementById('score-value').innerHTML = currentPlayersScore;
+        // if (currentPlayersScore > highscore) {
+        //     highscore = currentPlayersScore;
+        //     document.getElementById('high-score-value').innerHTML = currentPlayersScore;
+        // }
+        // var currentPlayerIndex = playerArray.length - 1;
+        // console.log(playerArray[currentPlayerIndex]);
+        // if (currentPlayersScore > playerArray[currentPlayerIndex].highscore) {
+        //
+        // }
+}
+
+
+
+
+// *****************************************************************************
 
 // Create a context for JS to play with (methods, properties, etc.)
 var canvas = document.createElement('canvas');
@@ -81,38 +110,40 @@ var backgroundImage = new Image();
 backgroundImage.src = "assets/background.png";
 
 // Initialize our characters
-
 var hero = new Hero("assets/hero.png", .5);
-var monster1 = new Monster("assets/monster.png");
-var monster2 = new Monster("assets/monster.png");
-var monster3 = new Monster("assets/monster.png");
+var keysDown = {};
+
 var monsters = [];
-monsters.push(monster1, monster2, monster3);
+monsters.push(
+    new Monster("assets/monster.png"),
+    new Monster("assets/monster.png"),
+    new Monster("assets/monster.png")
+);
 
+// Event listeners come first
+// We need a way to tell if the user has pushed an arrow key
+addEventListener('keydown', function(event) {
+    keysDown[event.keyCode] = true;
+});
 
-
-
-
-// Event listeners
-// addEventListener('keydown', function(event) {
-//     keysDown[event.keyCode] = true;
-// });
-//
-// addEventListener('keyup', function(event) {
-//     delete keysDown[event.keyCode];
-// });
+addEventListener('keyup', function(event) {
+    delete keysDown[event.keyCode];
+});
 
 
 function draw() {
 
+    hero.update(keysDown);
+
     context.drawImage(backgroundImage, 0, 0);
     context.drawImage(hero.image, hero.x, hero.y);
+
 
     for (var i = 0; i < monsters.length; i++) {
         context.drawImage(monsters[i].image, monsters[i].x, monsters[i].y);
         monsters[i].movement(1.5);
     }
-    
+
     requestAnimationFrame(draw);
 }
 
